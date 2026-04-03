3d310a0bb0263b029b9005a5c83f1df1e874d3f2
kwanho <kwanho8532@gmail.com>
2026-04-03 09:46:54 +0900

    axios setting modified

--- client/src/component/api/axios.js
+++ client/src/component/api/axios.js
@@ -3,7 +3,7 @@ import { useLoadingStore } from '../../store/useLoadingStore.js'
 import { useAuthStore } from '../../store/useAuthStore.js'
 
 const api = axios.create({
-    baseURL: '/api',
+    baseURL: import.meta.env.VITE_API_URL,
     withCredentials: true
 })
 
